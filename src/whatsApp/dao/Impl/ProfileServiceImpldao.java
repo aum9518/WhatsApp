7a72fc403fe943e2cccc62a4b9301f0aff952508
aum9518 <abduganiuulumanasbek@gmail.com>
2023-03-23 00:36:54 +0600

    add changedProfileStatus

--- src/whatsApp/dao/Impl/ProfileServiceImpldao.java
+++ src/whatsApp/dao/Impl/ProfileServiceImpldao.java
@@ -177,6 +177,22 @@ public class ProfileServiceImpldao implements ProfileServicedao {
                 isTrue = true;
                 if (a.getPassword().equals(password)){
                     isTrue1 = true;
+                    int counter = 0;
+                    for (Status n:Status.values()) {
+                        counter++;
+                        System.out.println(counter+"->"+n);
+                    }
+                    int num = new Scanner(System.in).nextInt();
+                    switch (num){
+                        case 1:a.setStatus(Status.I_AM_USING_WHATSAPP) ;break;
+                        case 2:a.setStatus(Status.TO_THE_CINEMA);break;
+                        case 3:a.setStatus(Status.BUSY);break;
+                        case 4:a.setStatus(Status.WORK);break;
+                        case 5:a.setStatus(Status.WORkOUT);break;
+                        default:
+                            System.out.println("Error");break;
+                    }
+                    return "Saccessfully changed";
                 }else {
                     isTrue1 = false;
                 }
@@ -199,16 +215,97 @@ public class ProfileServiceImpldao implements ProfileServicedao {
 
     @Override
     public void addNewContact(Group group) {
-
+        boolean isTrue = true;
+        boolean isTrue1 = true;
+        System.out.println("Enter user name:");
+        String name = new Scanner(System.in).nextLine();
+        System.out.println("Enter password");
+        String password = new Scanner(System.in).nextLine();
+        for (User a: group.getUsers()) {
+            if (a.getFirstName().equals(name)){
+                isTrue = true;
+                if (a.getPassword().equals(password)){
+                    isTrue1 = true;
+                }else {
+                    isTrue1 = false;
+                }
+            }else {
+                isTrue = false;
+            }
+        }
+        try{
+            if (!isTrue){
+                throw new MyException("Invalid user name!");
+            }
+            if (!isTrue1){
+                throw new MyException("Invalid password!");
+            }
+        } catch (MyException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Override
     public void sendMessage(Group group) {
-
+        boolean isTrue = true;
+        boolean isTrue1 = true;
+        System.out.println("Enter user name:");
+        String name = new Scanner(System.in).nextLine();
+        System.out.println("Enter password");
+        String password = new Scanner(System.in).nextLine();
+        for (User a: group.getUsers()) {
+            if (a.getFirstName().equals(name)){
+                isTrue = true;
+                if (a.getPassword().equals(password)){
+                    isTrue1 = true;
+                }else {
+                    isTrue1 = false;
+                }
+            }else {
+                isTrue = false;
+            }
+        }
+        try{
+            if (!isTrue){
+                throw new MyException("Invalid user name!");
+            }
+            if (!isTrue1){
+                throw new MyException("Invalid password!");
+            }
+        } catch (MyException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Override
     public void deleteWhatsApp(Group group) {
-
+        boolean isTrue = true;
+        boolean isTrue1 = true;
+        System.out.println("Enter user name:");
+        String name = new Scanner(System.in).nextLine();
+        System.out.println("Enter password");
+        String password = new Scanner(System.in).nextLine();
+        for (User a: group.getUsers()) {
+            if (a.getFirstName().equals(name)){
+                isTrue = true;
+                if (a.getPassword().equals(password)){
+                    isTrue1 = true;
+                }else {
+                    isTrue1 = false;
+                }
+            }else {
+                isTrue = false;
+            }
+        }
+        try{
+            if (!isTrue){
+                throw new MyException("Invalid user name!");
+            }
+            if (!isTrue1){
+                throw new MyException("Invalid password!");
+            }
+        } catch (MyException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
